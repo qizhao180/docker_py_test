6530eee78af2e80f6053182d5a690fc2b30bfe36
Qihua Zhao <qizhao@umich.edu>
2026-01-27 14:20:09 -0500

    first try

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.9.12
+#FROM python:3.7.7-slim
+
+COPY . /app
+
+WORKDIR /app
+
+RUN pip3 install -r requirements.txt
+
+EXPOSE 5000
+
+CMD ["python3", "app.py"]
---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import Response
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello world"
+
+@app.route("/healthz")
+def healthz():
+    resp = Response("ok")
+    resp.headers['Custom-Header'] = 'Awesome'
+    # this is awesome tying things
+    return resp
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
+    
